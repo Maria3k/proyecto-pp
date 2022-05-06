5247daa8c78e9d87029f1cbc1a7ec47a2ba75ede
Ignacio Rios Lahore <ignaciorioslahore@gmail.com>
2022-05-06 15:33:44 -0300

    Cambio total de HTML a PHP
    Cambié todos los hipervínculos y formularios html a PHP.

---
+++ automotores.php
@@ -0,0 +1,134 @@
+<?php
+include("conexion.php");
+session_start();
+
+if ($_SESSION) {
+  print_r($_SESSION);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="assets/img/favicon/favicon.png">
+  <link rel="stylesheet" href="assets/css/index.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/icons.css">
+  <link rel="stylesheet" href="assets/css/mi-carousel.css">
+  <link rel="stylesheet" href="assets/css/especialidades.css">
+  <title>Especialidad Automotores</title>
+</head>
+
+<body>
+  <nav>
+    <a href="index.php" title="Pagina Principal"><img class="img1" src="assets/img/escuela/loguito.png" alt="minilogo.png"></a>
+    <a class="btn-nav" href="register.php">Register</a><a class="btn-nav" href="login.php">Iniciar Sesion</a>
+  </nav>
+  <div class="container">
+    <div class="row">
+      <div class="col"></div>
+      <div class="col">
+        <div class="faq">
+          <h1>PREGUNTAS FRECUENTES</h1>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col">
+        <div class="pregunta">
+          <h1>DEJE SUS PREGUNTAS</h1>
+          <form action="">
+            <textarea name="" id="" cols="80" rows="10" placeholder="Ingrese su pregunta"></textarea>
+            <input type="submit" value="enviar">
+          </form>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>
+    <div class="card">
+      <div class="car-body">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <div class="row">
+      <div class="col l6">
+        <h5>Contacto</h5>
+        <ul>
+          <li>Email DOE: ofdealumnos32@gmail.com</li>
+          <li>Teléfonos: 4551-9121 4555-4026/4034</li>
+          <li>Dirección: Teodoro García 3899, C1427ECG CABA</li>
+          <li>
+            Autores:
+            <ul>
+              <li>Maria Morales - 6°1 Computacion</li>
+              <li>Nicolas Domeq - 6°1 Computacion</li>
+              <li>Ignacio Rios Lahore - 6°1 Computacion</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="col l6">
+        <div class="wrapper">
+          <div class="connect">
+            <a href="https://www.facebook.com/groups/tecnica32/" rel="noopener noreferrer" class="facebook" target="_blank" title="Facebook">
+              <!--LINK DE FACEBOOK -->
+              <i class="fab fa-facebook-f"></i>
+              <!--ICONO DE FACEBOOK -->
+            </a>
+
+            <a href="https://github.com/Maria3k/Proyecto-PP" rel="noopener noreferrer" class="github" target="_blank" title="Github">
+              <!--LINK DE GITHUB -->
+              <i class="fab fa-github"></i>
+              <!--ICONO DE GITHUB -->
+            </a>
+
+            <a href="https://www.youtube.com/channel/UCywUijMchnujSg6dVVUKprQ/videos?view_as=subscriber" rel="noopener noreferrer" class="youtube" target="_blank" title="Youtube">
+              <!--LINK DE YOUTUBE -->
+              <i class="fab fa-youtube"></i>
+              <!--ICONO DE YOUTUBE -->
+            </a>
+
+            <a href="https://www.instagram.com/la_gloriosa_32_escuela_tecnica/?hl=es-la" rel="noopener noreferrer" class="instagram" target="_blank" title="Instagram">
+              <!--LINK DE INSTAGRAM -->
+              <i class="fab fa-instagram"></i>
+              <!--ICONO DE INSTAGRAM -->
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
+</body>
+
+</html>
