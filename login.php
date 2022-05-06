c16e47b0e093be7883e6b3f28c54ca61f23bbd70
Ignacio Rios Lahore <ignaciorioslahore@gmail.com>
2022-05-06 15:33:01 -0300

    Subo un pequeño login con SESSION PHP
    -Hice un login PHP para guardar la session en la variable global de PHP $_SESSION.
    -Modifique el index para mostrar los datos de esa seción.
    -Cambié el tamaño del video de muestra del index.

--- login.php
+++ login.php
@@ -1,50 +1,80 @@
 <?php
-  include("conexion.php");
+include("conexion.php");
+$errEm = "";
+if (isset($_POST["submit"])) {
+  if (isset($_POST["email"])) {
+    if (isset($_POST["contraseña"])) {
+      $e = trim($_POST["email"]);
+      $p = trim($_POST["contraseña"]);
+
+      if (!empty($e)) {
+        if (!empty($p)) {
+          $query = "SELECT * FROM usuario WHERE email = '$e' AND contraseña = '$p'";
+          $send = $con->query($query);
+          $col = $send->fetch_assoc();
+
+          if (($send->num_rows) > 0) {
+            session_start();
+            $_SESSION = $col;
+            header("Location:index.php");
+          } else {
+
+            //$_SESSION[""] = $col[""];
+
+            $errEm = '<div style="color: #c30000;">Nombre de usuario o contraseña incorrectos</div>';
+          }
+        }
+      }
+    }
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="assets/img/favicon/favicon.png">
-    <link rel="stylesheet" href="assets/css/login.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <title>Iniciar Sesión</title>
-  </head>
 
-  <body>
-    <div id="formulario" class="container">
-      <div class="row">
-        <div id="logo" class="col-4">
-          <img src="assets/img/iconos/logoblanco.png" alt="logoblanco.png" class="logo_main">
-        </div>
-        <div class="col-8" >
-          <div id="form" class="row" style="padding: 20px;">
-            <div class="col-12">
-              <h2 style="text-align: center;">INICIAR SESION</h2>
-            </div>
-            <div class="col-12">
-              <form action="login.html" method="post">
-                <div class="form-group">
-                  <input type="email" name="email" placeholder="Ingrese su correo electronico" required>
-                </div>
-                <div class="form-group">
-                  <input type="password" name="contraseña" placeholder="Ingrese su contraseña" required>
-                  <input type="checkbox" name="recordar" style="width:auto;">Recordar nombre de usuario
-                </div>
-                <div class="form-group">
-                  <input id="submit" type="submit" name="submit" value="Ingresar">
-                </div>
-              </form>
-              <a class="reg" href="register.html">Crear Cuenta</a>
-              <a class="forg" href="forgot-password.html">¿Olvidaste tu contraseña?</a>
-            </div>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="assets/img/favicon/favicon.png">
+  <link rel="stylesheet" href="assets/css/login.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <title>Iniciar Sesión</title>
+</head>
+
+<body>
+  <div id="formulario" class="container">
+    <div class="row">
+      <div id="logo" class="col-4">
+        <img src="assets/img/iconos/logoblanco.png" alt="logoblanco.png" class="logo_main">
+      </div>
+      <div class="col-8">
+        <div id="form" class="row" style="padding: 20px;">
+          <div class="col-12">
+            <h2 style="text-align: center;">INICIAR SESION</h2>
+          </div>
+          <div class="col-12">
+            <form action="login.php" method="post">
+              <div class="form-group">
+                <input type="email" name="email" placeholder="Ingrese su correo electronico" required>
+                <?php echo $errEm; ?>
+              </div>
+              <div class="form-group">
+                <input type="password" name="contraseña" placeholder="Ingrese su contraseña" required>
+                <span>Recordar nombre de usuario</span><input type="checkbox" name="recordar">
+              </div>
+              <div class="form-group">
+                <input id="submit" type="submit" name="submit" value="Ingresar">
+              </div>
+            </form>
+            <a class="reg" href="register.html">Crear Cuenta</a>
+            <a class="forg" href="forgot-password.html">¿Olvidaste tu contraseña?</a>
           </div>
         </div>
       </div>
     </div>
-    <script src="assets/js/bootstrap.js"></script>
-  </body>
+  </div>
+  <script src="assets/js/bootstrap.js"></script>
+</body>
 
 </html>
