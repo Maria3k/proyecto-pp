7184db6fcc0bceeb8b3e1a3021e86622d0ad74d9
Doumeq <f.nicolasdomeq@gmail.com>
2022-07-06 17:48:33 -0300

    modificacion completa de las vistas del perfil
    remplacé y empecé las vistas del perfil nuevas

--- infoPersonal.php
+++ infoPersonal.php
@@ -1,171 +1,76 @@
-<?php
-include("conexion.php");
-session_start();
-
-$nAvatar = $_SESSION["nAvatar"];
-
-$query = "SELECT * FROM avatar WHERE id_avatar = $nAvatar";
-$send = $con->query($query);
-$col = $send->fetch_assoc();
-
-if (isset($_GET['name'])){
-    $con->query("DELETE FROM usuario WHERE email = '" . $_SESSION["email"]."'") or die("ERROR SQL ->".$con->error);
-    session_destroy();
-    header("Location:index.php");
-}
-
-?>
-
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="assets/img/favicon/favicon.png">
-    <link rel="stylesheet" href="assets/css/perfil.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/icons.css">
-    <link rel="stylesheet" href="assets/css/especialidades.css">
-    <link rel="stylesheet" href="assets/css/infoPersonal.css">
-    <title>Informacion Personal - Perfil</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="assets/js/"></script>
 </head>
-
 <body>
-    <div class="container">
-        <div class="row">
-            <div id="menu" class="col-3">
-                <ul class="letra">
-                    <a href="perfil.php">
-                        <li>
-                            <i class="fa-solid fa-circle-user"></i>   Inicio
-                        </li>
-                    </a>
-                    <li id="selec">
-                        <!--PUNTO DE REFERENCIA-->
-                        <span><i class="fa-solid fa-id-card"></i>   Información personal</span>
-                    </li>
-                    <a href="seguridad.php">
-                        <li>
-                            <i class="fa-solid fa-screwdriver-wrench"></i>   Seguridad
-                        </li>
-                    </a>
-                    <a href="infoGeneral.php">
-                        <li class="ult">
-                            <i class="fa-solid fa-circle-exclamation"></i>   Información general
-                        </li>
-                    </a>
-                </ul>
-            </div>
-            <div id="section" class="col-9">
-                <div id="datos1" class="col-9">
-                    <img src=<?= $col["rutaArchivo"]; ?> width="100px" height="100px" alt=<?= $col["nombreArchivo"]; ?>>
-                    <ul>
-                        <li>Nombre: <?= $_SESSION["nombre"], " ", $_SESSION["apellido"]; ?></li>
-                        <li>Fecha de nacimiento: <?= $_SESSION["fechaNacimiento"] ?></li>
-                    </ul>
-                </div>
-                <br>
-                <div id="datos2" class="col-9">
-                    <ul>
-                        <li>Correo electronico: <?= $_SESSION["email"];  ?></li>
-                        <li>Telefono: <?= $_SESSION["telefono"];  ?></li>
-                    </ul>
-                </div>
-                <div id="boton1">
-                    <input id="pass" class="form-control" type="password" name="pwd" value="<?= $_SESSION["contraseña"];  ?>" placeholder="contraseña" readonly>
-                    <i id="mostrar" class="fas fa-eye" onclick="myFunction()"></i>
-                </div>
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Eliminar Cuenta
-                </button>
+    <div id="accordian">
+        <ul class="show-dropdown main-navbar">
+        <div class="selector-active"><div class="top"></div><div class="bottom"></div></div>
+        <li>
+        <a href="#" onclick="perfil()"><i class="fa fa-tachometer"></i>Inicio</a>
+        </li>
+        <li class="active">
+        <a href="#"><i class="fa fa-address-book"></i>Información Personal</a>
+        </li>
+        <li>
+        <a href="#" onclick="seguridad()"><i class="fa fa-clone"></i>Seguridad</a>
+        </li>
+        <li>
+        <a href="#" onclick="infoGeneral()"><i class="fa fa-calendar"></i>Información General</a>
+        </li>
+        </ul>
+       </div>
+       <script>
+           // ---------vertical-menu with-inner-menu-active-animation-----------.
+        var tabsVerticalInner = $('#accordian');
+        var selectorVerticalInner = $('#accordian').find('li').length;
+        var activeItemVerticalInner = tabsVerticalInner.find('.active');
+        var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
+        var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
+        var itemPosVerticalTop = activeItemVerticalInner.position();
+        var itemPosVerticalLeft = activeItemVerticalInner.position();
+        $(".selector-active").css({
+        "top":itemPosVerticalTop.top + "px", 
+        "left":itemPosVerticalLeft.left + "px",
+        "height": activeWidthVerticalHeight + "px",
+        "width": activeWidthVerticalWidth + "px"
+        });
+        $("#accordian").on("click","li",function(e){
+        $('#accordian ul li').removeClass("active");
+        $(this).addClass('active');
+        var activeWidthVerticalHeight = $(this).innerHeight();
+        var activeWidthVerticalWidth = $(this).innerWidth();
+        var itemPosVerticalTop = $(this).position();
+        var itemPosVerticalLeft = $(this).position();
+        $(".selector-active").css({
+            "top":itemPosVerticalTop.top + "px", 
+            "left":itemPosVerticalLeft.left + "px",
+            "height": activeWidthVerticalHeight + "px",
+            "width": activeWidthVerticalWidth + "px"
+        });
+        });
 
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Eliminar cuenta</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Estas seguro de eliminar tu cuenta?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                                <a href='infoPersonal.php?name=true' class="btn btn-danger">Confirmar</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <footer>
-        <div id="row" class="row">
-            <div class="col l6">
-                <h5>Contacto</h5>
-                <ul>
-                    <li>Email DOE: ofdealumnos32@gmail.com</li>
-                    <li>Teléfonos: 4551-9121 4555-4026/4034</li>
-                    <li>Dirección: Teodoro García 3899, C1427ECG CABA</li>
-                    <li>
-                        Autores:
-                        <ul>
-                            <li>Maria Morales - 6°1 Computacion</li>
-                            <li>Nicolas Domeq - 6°1 Computacion</li>
-                            <li>Ignacio Rios Lahore - 6°1 Computacion</li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <div class="col l6">
-
-                <div class="wrapper">
-                    <div class="connect">
-                        <a href="https://www.facebook.com/groups/tecnica32/" rel="noopener noreferrer" class="facebook" target="_blank" title="Facebook">
-                            <!--LINK DE FACEBOOK -->
-                            <i class="fab fa-facebook-f"></i>
-                            <!--ICONO DE FACEBOOK -->
-                        </a>
-
-                        <a href="https://github.com/Maria3k/Proyecto-PP" rel="noopener noreferrer" class="github" target="_blank" title="Github">
-                            <!--LINK DE GITHUB -->
-                            <i class="fab fa-github"></i>
-                            <!--ICONO DE GITHUB -->
-                        </a>
-
-                        <a href="https://www.youtube.com/channel/UCywUijMchnujSg6dVVUKprQ/videos?view_as=subscriber" rel="noopener noreferrer" class="youtube" target="_blank" title="Youtube">
-                            <!--LINK DE YOUTUBE -->
-                            <i class="fab fa-youtube"></i>
-                            <!--ICONO DE YOUTUBE -->
-                        </a>
-
-                        <a href="https://www.instagram.com/la_gloriosa_32_escuela_tecnica/?hl=es-la" rel="noopener noreferrer" class="instagram" target="_blank" title="Instagram">
-                            <!--LINK DE INSTAGRAM -->
-                            <i class="fab fa-instagram"></i>
-                            <!--ICONO DE INSTAGRAM -->
-                        </a>
-                    </div>
-                </div>
-            </div>
-    </footer>
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
-    <script>
-        function myFunction() {
-            var x = document.getElementById("pass");
-            if (x.type === "password") {
-                x.type = "text";
-                document.getElementById("mostrar").setAttribute("class", "fas fa-eye-slash");
-            } else {
-                x.type = "password";
-                document.getElementById("mostrar").setAttribute("class", "fas fa-eye");
-            }
+        function seguridad(){
+          setTimeout("location.href = 'seguridad.php';",200);
         }
-    </script>
-</body>
 
+        function perfil(){
+          setTimeout("location.href = 'perfil.php';",200);
+        }
+        
+        function infoGeneral(){
+          setTimeout("location.href = 'infoGeneral.php';",200);
+        }
+        
+        
+       </script>
+</body>
 </html>
--- perfil.php
+++ perfil.php
@@ -1,125 +1,74 @@
-<?php
-include("conexion.php");
-session_start();
-
-
-$nAvatar = $_SESSION["nAvatar"];
-
-$query = "SELECT * FROM avatar WHERE id_avatar = $nAvatar";
-$send = $con->query($query);
-$col = $send->fetch_assoc();
-
-
-?>
-
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="assets/img/favicon/favicon.png">
-  <link rel="stylesheet" href="assets/css/perfil.css">
-  <link rel="stylesheet" href="assets/css/bootstrap.css">
-  <link rel="stylesheet" href="assets/css/icons.css">
-  <link rel="stylesheet" href="assets/css/especialidades.css">
-  <title>Inicio - Perfil</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="assets/js/"></script>
 </head>
-
 <body>
-  <div class="container">
-    <div class="row">
-      <div id="menu" class="col-3">
-        <ul class="letra">
-          <li id="selec">
-            <!--PUNTO DE REFERENCIA-->
-            <span><i class="fa-solid fa-circle-user"></i>   Inicio</span>
-          </li>
-          <a href="infoPersonal.php">
-            <li>
-              <i class="fa-solid fa-id-card"></i>   Información personal
-            </li>
-          </a>
-          <a href="seguridad.php">
-            <li>
-              <i class="fa-solid fa-screwdriver-wrench"></i>   Seguridad
-            </li>
-          </a>
-          <a href="infoGeneral.php">
-            <li class="ult">
-              <i class="fa-solid fa-circle-exclamation"></i>   Información general
-            </li>
-          </a>
+    <div id="accordian">
+        <ul class="show-dropdown main-navbar">
+        <div class="selector-active"><div class="top"></div><div class="bottom"></div></div>
+        <li class="active">
+        <a href="#"><i class="fa fa-tachometer"></i>Inicio</a>
+        </li>
+        <li>
+        <a href="#" onclick="infoPersonal()"><i class="fa fa-address-book"></i>Información Personal</a>
+        </li>
+        <li>
+        <a href="#" onclick="seguridad()"><i class="fa fa-clone"></i>Seguridad</a>
+        </li>
+        <li>
+        <a href="#" onclick="infoGeneral()"><i class="fa fa-calendar"></i>Información General</a>
+        </li>
         </ul>
-      </div>
-      <div id="section" class="col-9">
-        <div class="principal">
-          <img src=<?= $col["rutaArchivo"]; ?> width="100px" height="100px" alt=<?= $col["nombreArchivo"]; ?>>
-          <h2>¡Bienvenido <?= $_SESSION["nickname"] ?>!</h2>
-        </div>
-        <br>
-        <div class="boton">
-          <a href="">Consultas</a>
-        </div>
-        <br><br>
-        <div class="boton">
-          <a href="">Enviar Comentarios</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <footer>
-    <div class="row">
-      <div class="col l6">
-        <h5>Contacto</h5>
-        <ul>
-          <li>Email DOE: ofdealumnos32@gmail.com</li>
-          <li>Teléfonos: 4551-9121 4555-4026/4034</li>
-          <li>Dirección: Teodoro García 3899, C1427ECG CABA</li>
-          <li>
-            Autores:
-            <ul>
-              <li>Maria Morales - 6°1 Computacion</li>
-              <li>Nicolas Domeq - 6°1 Computacion</li>
-              <li>Ignacio Rios Lahore - 6°1 Computacion</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div class="col l6">
-        <div class="wrapper">
-          <div class="connect">
-            <a href="https://www.facebook.com/groups/tecnica32/" rel="noopener noreferrer" class="facebook" target="_blank" title="Facebook">
-              <!--LINK DE FACEBOOK -->
-              <i class="fab fa-facebook-f"></i>
-              <!--ICONO DE FACEBOOK -->
-            </a>
+       </div>
+       <script>
+           // ---------vertical-menu with-inner-menu-active-animation-----------.
+        var tabsVerticalInner = $('#accordian');
+        var selectorVerticalInner = $('#accordian').find('li').length;
+        var activeItemVerticalInner = tabsVerticalInner.find('.active');
+        var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
+        var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
+        var itemPosVerticalTop = activeItemVerticalInner.position();
+        var itemPosVerticalLeft = activeItemVerticalInner.position();
+        $(".selector-active").css({
+        "top":itemPosVerticalTop.top + "px", 
+        "left":itemPosVerticalLeft.left + "px",
+        "height": activeWidthVerticalHeight + "px",
+        "width": activeWidthVerticalWidth + "px"
+        });
+        $("#accordian").on("click","li",function(e){
+        $('#accordian ul li').removeClass("active");
+        $(this).addClass('active');
+        var activeWidthVerticalHeight = $(this).innerHeight();
+        var activeWidthVerticalWidth = $(this).innerWidth();
+        var itemPosVerticalTop = $(this).position();
+        var itemPosVerticalLeft = $(this).position();
+        $(".selector-active").css({
+            "top":itemPosVerticalTop.top + "px", 
+            "left":itemPosVerticalLeft.left + "px",
+            "height": activeWidthVerticalHeight + "px",
+            "width": activeWidthVerticalWidth + "px"
+        });
+        });
 
-            <a href="https://github.com/Maria3k/Proyecto-PP" rel="noopener noreferrer" class="github" target="_blank" title="Github">
-              <!--LINK DE GITHUB -->
-              <i class="fab fa-github"></i>
-              <!--ICONO DE GITHUB -->
-            </a>
+        function seguridad(){
+          setTimeout("location.href = 'seguridad.php';",200);
+        }
+        
+        function infoPersonal(){
+          setTimeout("location.href = 'infoPersonal.php';",200);
+        }
 
-            <a href="https://www.youtube.com/channel/UCywUijMchnujSg6dVVUKprQ/videos?view_as=subscriber" rel="noopener noreferrer" class="youtube" target="_blank" title="Youtube">
-              <!--LINK DE YOUTUBE -->
-              <i class="fab fa-youtube"></i>
-              <!--ICONO DE YOUTUBE -->
-            </a>
-
-            <a href="https://www.instagram.com/la_gloriosa_32_escuela_tecnica/?hl=es-la" rel="noopener noreferrer" class="instagram" target="_blank" title="Instagram">
-              <!--LINK DE INSTAGRAM -->
-              <i class="fab fa-instagram"></i>
-              <!--ICONO DE INSTAGRAM -->
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <script src="assets/js/bootstrap.js"></script>
-  <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
+        function infoGeneral(){
+          setTimeout("location.href = 'infoGeneral.php';",200);
+        }
+       </script>
 </body>
-
 </html>
