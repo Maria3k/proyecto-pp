6dfd23885d82bc4851e1d9f5f56cb15ce931078c
IvanBritez <nahuelbtz@gmail.com>
2022-07-12 15:44:32 -0300

    Rediseño de perfil

--- index.php
+++ index.php
@@ -1,161 +1,156 @@
+<?php
+include("conexion.php");
+session_start();
+
+$menu = '';
+
+if ($_SESSION) {
+
+  $nAvatar = $_SESSION["nAvatar"];
+
+  $send = $con->query("SELECT * FROM avatar WHERE id_avatar = $nAvatar");
+  $col = $send->fetch_assoc();
+  $ad = '';
+
+  if($_SESSION["rol"] == 2){
+    $ad = '<li><a href="backend.php"><i class="fa-solid fa-gear"></i>Editar</a></li>';
+  }
+
+
+  $menu = '        
+  <div class="action" >
+    <div class="profile" onclick="menuToggle()">
+      <img src="'.$col["rutaArchivo"].'" width="30px" height="30px" alt="'.$col["nombreArchivo"].'">
+    </div>
+    <div class="menu">
+      <ul>
+        <li id="primero"><a href="perfil.php"><i class="fa-solid fa-user"></i>Perfil</a></li>
+        '.$ad.'
+        <li><a href="logout.php"><i class="fa-solid fa-arrow-right-from-bracket"></i>Cerrar Sesion</a></li>
+      </ul>
+    </div>
+  </div>
+        ';
+} else {
+  $menu = '<a class="btn-nav" href="register.php">Register</a><a class="btn-nav" href="login.php">Iniciar Sesion</a>';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="assets/img/favicon/favicon.png">
-    <link rel="stylesheet" href="assets/css/index.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/icons.css">
-    <link rel="stylesheet" href="assets/css/especialidades.css">
-    <script src="https://kit.fontawesome.com/b3b892b65b.js" crossorigin="anonymous"></script>
-    <title>E.T.32 Respuestas</title>
-  </head>
-  <body>
-    <nav>
-      <img class="img1" src="assets/img/escuela/loguito.png" alt="loguito.png">
-      <a class="btn-nav" href="register.html">Register</a>
-      <a class="btn-nav" href="login.html">Iniciar Sesion</a>
-      <div class="action" >
-        <div class="profile" onclick="menuToggle()">
-          <img src="assets/img/iconosUsu/logo1.png" width="30px" height="30px" alt="usuario">
-        </div>
-        <div class="menu">
-          <ul>
-            <li id="primero"><a href="perfil.html"><i class="fa-solid fa-user"></i>Perfil</a></li>
-            <li><a href="perfil.html"><i class="fa-solid fa-gear"></i>Configuracion</a></li>
-            <li><a href="perfil.html"><i class="fa-solid fa-arrow-right-from-bracket"></i>Cerrar Sesion</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <h1>HISTORIA DE LA <span>ET32</span></h1>     
-    <br>
-    <div class="process-wrapper">
-      <div id="progress-bar-container">
-        <ul>
-          <li class="step step01 active">
-            <div class="step-inner">ESCUELA FÁBRICA</div>
-          </li>
-          <li class="step step02">
-            <div class="step-inner">ACTO INAUGURAL</div>
-          </li>
-          <li class="step step03">
-            <div class="step-inner">ESCUELA MUNICIPAL</div>
-            </li>
-            <li class="step step04">
-              <div class="step-inner">ESCUELA TÉCNICA</div>
-            </li>
-            <li class="step step05">
-              <div class="step-inner">LEMA DE LA ET32</div>
-            </li>
-        </ul>
-        <div id="line">
-          <div id="line-process"></div>
-        </div>
-      </div>
 
-      <div id="progress-content-section">
-        <div class="section-content discovery active">
-          <h2>Escuela Fábrica</h2>
-          <p>
-            En agosto de 1950 se inaugura como Escuela Fábrica 131 "Géneral José De San Martín".
-          </p>
-        </div>
-        <div class="section-content strategy">
-          <h2>Acto Inaugural</h2>
-          <p>
-            22 de agosto de 1950 se realizó el acto inaugural, con la presencia del entonces Presidente de la Nación, General Juan Domingo Perón. 
-          </p>
-        </div>
-        <div class="section-content crative">
-          <h2>Escuela Municipal</h2>
-          <p>
-            En 1992 la escuela pasó a la Administración del ámbito de la Municipalidad de la Ciudad de Buenos Aires cambiando su denominación a Escuela Municipal de Enseñanza Técnica 2 "General José de San Martín".
-          </p>
-        </div>
-        <div class="section-content production">
-          <h2>Escuela Técnica</h2>
-          <p>
-            En 1994 pasó a ser Escuela Técnica 32 Distrito Escolar 14 "General José de San Martín" hasta la actualidad.
-          </p>
-        </div>
-        <div class="section-content analysis">
-          <h2>Lema de la ET32</h2>
-          <p>
-            Somos el azul de nuestros guardapolvos de Técnica; somos el equipo: indestructibles engranajes metálicos unidos en eterno movimiento; somos vivos rayos, energía que dan potencia y dinamismo a magna labor, somos el volante que rodea y direcciona al objetivo; somos la placa impresa, base que sostiene y que transmite plenas conexiones para que todo funcione, somos escuela pública y técnic allí donde reside la patria misma... !somos la 32¡
-          </p>
-        </div>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:title" content="Facundo se la come" />
+  <meta property="og:image" content="http://186.23.120.80:800/pagina/Proyecto-PP/assets/img/iconos/logoblanco.png" />
+  <meta property="og:description" content="E.T.N°32 D.E.N°14 Preguntas y Respuestas es E.T.N°32 D.E.N°14 Preguntas y Respuestas..... XD">
+  <meta property="og:image:width" content="300px" />
+  <meta property="og:image:height" content="300px" />
+  <link rel="icon" href="assets/img/favicon/favicon.png">
+  <link rel="stylesheet" href="assets/css/index.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/icons.css">
+  <link rel="stylesheet" href="assets/css/especialidades.css">
+  <title>E.T.32 Preguntas</title>
+</head>
+
+<body>
+  <nav>
+    <img class="img1" src="assets/img/escuela/loguito.png" alt="loguito.png">
+    <div class="icon-wrapper" data-numbrer="1">
+      <img src="assets/img/iconos/bell.png" class="bell-icon">
+    </div>
+    <?= $menu ?>
+  </nav>
+  <div class="container">
+    <div class="row align-item-center">
+      <div class="col-4">
+        <img class="img-fluid" src="assets/img/escuela/escuela.jpg" alt="escuela.jpg">
       </div>
-    </div>    
-    <div class="Contenedor">
-      <div class="card">
-        <a href="computacion.html" title="Sección de Computacion"><img src="assets/img/escuela/computacion.png" alt="computacion.png"></a>
+      <div class="col-6">
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus mollitia similique non atque nesciunt velit recusandae a reprehenderit cum enim quasi perferendis aperiam dolorum magnam, aliquam laudantium quidem, animi qui. </p>
+      </div>
+    </div>
+  </div>
+  <div class="Contenedor">
+    <div class="card">
+      <a href="computacion.php" title="Sección de Computacion"><img src="assets/img/escuela/computacion.png" alt="computacion.png"></a>
       <h1>Computacion</h1>
-      </div>
+    </div>
 
-      <div class="card">
-        <a href="mecanica.html" title="Sección de Mecanica"><img src="assets/img/escuela/mecanica.png" alt="mecanica.png"></a>
+    <div class="card">
+      <a href="mecanica.php" title="Sección de Mecanica"><img src="assets/img/escuela/mecanica.png" alt="mecanica.png"></a>
       <h1>Mecanica</h1>
-      </div>
+    </div>
 
-      <div class="card">
-        <a href="automotores.html" title="Sección de Automotores"><img src="assets/img/escuela/automotores.png" alt="automotores.png"></a>
+    <div class="card">
+      <a href="automotores.php" title="Sección de Automotores"><img src="assets/img/escuela/automotores.png" alt="automotores.png"></a>
       <h1>Automotores</h1>
+    </div>
+  </div>
+  <div class="contenedorVideo">
+    <video src="assets/video/3 Ejercicios Para Saltar Mas En Casa(Prueba).mp4" class="slider" loop muted controls></video>
+  </div>
+  <footer>
+    <div class="row">
+      <div class="col l6">
+        <h5>Contacto</h5>
+        <ul>
+          <li>Email DOE: ofdealumnos32@gmail.com</li>
+          <li>Teléfonos: 4551-9121 4555-4026/4034</li>
+          <li>Dirección: Teodoro García 3899, C1427ECG CABA</li>
+          <li>
+            Autores:
+            <ul>
+              <li>Maria Morales - 6°1 Computacion</li>
+              <li>Nicolas Domeq - 6°1 Computacion</li>
+              <li>Ignacio Rios Lahore - 6°1 Computacion</li>
+            </ul>
+          </li>
+        </ul>
       </div>
-    </div>
-    <div class="contenedorVideo">
-      <video src="assets/video/3 Ejercicios Para Saltar Mas En Casa(Prueba).mp4" class="slider" loop muted controls></video>
-    </div>
-    <footer>
-      <div class="row">
-        <div class="col l6">
-          <h5>Contacto</h5>
-          <ul>
-            <li>Email DOE: ofdealumnos32@gmail.com</li>
-            <li>Teléfonos: 4551-9121 4555-4026/4034</li>
-            <li>Dirección: Teodoro García 3899, C1427ECG CABA</li>
-            <li>
-              Autores:
-              <ul>
-                <li>Maria Morales - 6°1 Computacion</li>
-                <li>Nicolas Domeq - 6°1 Computacion</li>
-                <li>Ignacio Rios Lahore - 6°1 Computacion</li>
-                <li>Thristal Guerra - 6°1 Computacion</li>
-              </ul>
-            </li>
-          </ul>
-        </div>       
-        <div class="col l6">
-          <div class="wrapper">
-            <div class="connect">
-              <a href="https://www.facebook.com/groups/tecnica32/" rel="noopener noreferrer" class="facebook" target="_blank" title="Facebook"><!--LINK DE FACEBOOK -->
-                <i class="fab fa-facebook-f"></i> <!--ICONO DE FACEBOOK -->
-              </a>
-            
-              <a href="https://github.com/Maria3k/Proyecto-PP" rel="noopener noreferrer" class="github" target="_blank" title="Github"><!--LINK DE GITHUB -->
-                <i class="fab fa-github"></i> <!--ICONO DE GITHUB -->
-              </a>
-            
-              <a href="https://www.youtube.com/channel/UCywUijMchnujSg6dVVUKprQ/videos?view_as=subscriber" rel="noopener noreferrer" class="youtube" target="_blank" title="Youtube"><!--LINK DE YOUTUBE -->
-                <i class="fab fa-youtube"></i> <!--ICONO DE YOUTUBE -->
-              </a>
+      <div class="col l6">
+        <div class="wrapper">
+          <div class="connect">
+            <a href="https://www.facebook.com/groups/tecnica32/" rel="noopener noreferrer" class="facebook" target="_blank" title="Facebook">
+              <!--LINK DE FACEBOOK -->
+              <i class="fab fa-facebook-f"></i>
+              <!--ICONO DE FACEBOOK -->
+            </a>
 
-              <a href="https://www.instagram.com/la_gloriosa_32_escuela_tecnica/?hl=es-la" rel="noopener noreferrer" class="instagram" target="_blank" title="Instagram"><!--LINK DE INSTAGRAM -->
-                <i class="fab fa-instagram"></i><!--ICONO DE INSTAGRAM -->
-              </a>
-            </div>
+            <a href="https://github.com/Maria3k/Proyecto-PP" rel="noopener noreferrer" class="github" target="_blank" title="Github">
+              <!--LINK DE GITHUB -->
+              <i class="fab fa-github"></i>
+              <!--ICONO DE GITHUB -->
+            </a>
+
+            <a href="https://www.youtube.com/channel/UCywUijMchnujSg6dVVUKprQ/videos?view_as=subscriber" rel="noopener noreferrer" class="youtube" target="_blank" title="Youtube">
+              <!--LINK DE YOUTUBE -->
+              <i class="fab fa-youtube"></i>
+              <!--ICONO DE YOUTUBE -->
+            </a>
+
+            <a href="https://www.instagram.com/la_gloriosa_32_escuela_tecnica/?hl=es-la" rel="noopener noreferrer" class="instagram" target="_blank" title="Instagram">
+              <!--LINK DE INSTAGRAM -->
+              <i class="fab fa-instagram"></i>
+              <!--ICONO DE INSTAGRAM -->
+            </a>
           </div>
         </div>
       </div>
-    </footer>
-    <script src="assets/js/bootstrap.js"></script>
-    <script>
-      function menuToggle(){
-        const toggleMenu = document.querySelector('.menu');
-        toggleMenu.classList.toggle('active')    
-      }
-    </script>
-  </body>
+    </div>
+  </footer>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
+  <script>
+    function menuToggle() {
+      const toggleMenu = document.querySelector('.menu');
+      toggleMenu.classList.toggle('active')
+    }
+  </script>
+</body>
+
 </html>
