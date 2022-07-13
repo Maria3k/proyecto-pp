101040ebcb1fab50b2254c574ee87685bea9054d
IvanBritez <nahuelbtz@gmail.com>
2022-07-13 17:22:19 -0300

    Plantilla para la sección Usuario

--- perfil.php
+++ perfil.php
@@ -8,7 +8,8 @@
     <link rel="stylesheet" href="assets/css/perfil.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="assets/js/"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/icons.css">
 </head>
 <body>
     <div id="accordian">
@@ -51,6 +52,8 @@
               <li>Maria Morales - 6°1 Computacion</li>
               <li>Nicolas Domeq - 6°1 Computacion</li>
               <li>Ignacio Rios Lahore - 6°1 Computacion</li>
+              <li>Thristall Guerra - 6°1 Computacion</li>
+              <li>Ivan Britez - 6°1 Computacion</li>
             </ul>
           </li>
         </ul>
@@ -87,47 +90,49 @@
     </div>
   </footer>
 
-       <script>
-           // ---------vertical-menu with-inner-menu-active-animation-----------.
-        var tabsVerticalInner = $('#accordian');
-        var selectorVerticalInner = $('#accordian').find('li').length;
-        var activeItemVerticalInner = tabsVerticalInner.find('.active');
-        var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
-        var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
-        var itemPosVerticalTop = activeItemVerticalInner.position();
-        var itemPosVerticalLeft = activeItemVerticalInner.position();
-        $(".selector-active").css({
-        "top":itemPosVerticalTop.top + "px",
-        "left":itemPosVerticalLeft.left + "px",
-        "height": activeWidthVerticalHeight + "px",
-        "width": activeWidthVerticalWidth + "px"
-        });
-        $("#accordian").on("click","li",function(e){
-        $('#accordian ul li').removeClass("active");
-        $(this).addClass('active');
-        var activeWidthVerticalHeight = $(this).innerHeight();
-        var activeWidthVerticalWidth = $(this).innerWidth();
-        var itemPosVerticalTop = $(this).position();
-        var itemPosVerticalLeft = $(this).position();
-        $(".selector-active").css({
-            "top":itemPosVerticalTop.top + "px",
-            "left":itemPosVerticalLeft.left + "px",
-            "height": activeWidthVerticalHeight + "px",
-            "width": activeWidthVerticalWidth + "px"
-        });
-        });
+  <script>
+      // ---------vertical-menu with-inner-menu-active-animation-----------.
+  var tabsVerticalInner = $('#accordian');
+  var selectorVerticalInner = $('#accordian').find('li').length;
+  var activeItemVerticalInner = tabsVerticalInner.find('.active');
+  var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
+  var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
+  var itemPosVerticalTop = activeItemVerticalInner.position();
+  var itemPosVerticalLeft = activeItemVerticalInner.position();
+  $(".selector-active").css({
+  "top":itemPosVerticalTop.top + "px",
+  "left":itemPosVerticalLeft.left + "px",
+  "height": activeWidthVerticalHeight + "px",
+  "width": activeWidthVerticalWidth + "px"
+  });
+  $("#accordian").on("click","li",function(e){
+  $('#accordian ul li').removeClass("active");
+  $(this).addClass('active');
+  var activeWidthVerticalHeight = $(this).innerHeight();
+  var activeWidthVerticalWidth = $(this).innerWidth();
+  var itemPosVerticalTop = $(this).position();
+  var itemPosVerticalLeft = $(this).position();
+  $(".selector-active").css({
+      "top":itemPosVerticalTop.top + "px",
+      "left":itemPosVerticalLeft.left + "px",
+      "height": activeWidthVerticalHeight + "px",
+      "width": activeWidthVerticalWidth + "px"
+  });
+  });
 
-        function seguridad(){
-          setTimeout("location.href = 'seguridad.php';",400);
-        }
+  function seguridad(){
+    setTimeout("location.href = 'seguridad.php';",400);
+  }
 
-        function infoPersonal(){
-          setTimeout("location.href = 'infoPersonal.php';",400);
-        }
+  function infoPersonal(){
+    setTimeout("location.href = 'infoPersonal.php';",400);
+  }
 
-        function infoGeneral(){
-          setTimeout("location.href = 'infoGeneral.php';",400);
-        }
-       </script>
+  function infoGeneral(){
+    setTimeout("location.href = 'infoGeneral.php';",400);
+  }
+  </script>
+  <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
