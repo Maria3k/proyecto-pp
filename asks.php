3b6360ea56e0e85591a91d4b5a92997f3703ee4b
Pistak41 <ignaciorioslahore@gmail.com>
2022-08-03 17:08:06 -0300

    Subo pueba de los nuevos estilos del foro

---
+++ asks.php
@@ -0,0 +1,45 @@
+<?php
+
+include("conexion.php");
+session_start();
+
+if ($_POST) {
+  if (isset($_POST["clave"])) {
+    $query = $con->query(
+      "SELECT pregunta.*, usuario.*, avatar.* FROM pregunta LEFT JOIN usuario ON pregunta.usuario_pregunta = usuario.id_usuario LEFT JOIN avatar ON usuario.nAvatar = avatar.id_avatar WHERE especialidad = " . $_POST["e"] . " AND asunto LIKE '%" . $_POST["clave"] . "%'") or die($query . mysqli_error($con));
+    $json = array();
+    while ($ask = $query->fetch_assoc()) {
+      $json[] = array(
+        "id" => $ask["id_pregunta"],
+        "nombre" => $ask["nombre"],
+        "apellido" => $ask["apellido"],
+        "nickname" => $ask["nickname"],
+        "avatar" => $ask["rutaArchivo"],
+        "avatarName" => $ask["nombreArchivo"],
+        "asunto" => $ask["asunto"],
+        "contenido" => $ask["contenido"],
+        "fecha" => $ask["fechaPreguntada"],
+      );
+    }
+    echo json_encode($json);
+  }else{
+    $query = $con->query("SELECT pregunta.*, usuario.*, avatar.* FROM pregunta LEFT JOIN usuario ON pregunta.usuario_pregunta = usuario.id_usuario LEFT JOIN avatar ON usuario.nAvatar = avatar.id_avatar WHERE especialidad = " . $_POST["e"])or die($query.mysqli_error($con));
+    $json = array();
+    while ($ask = $query->fetch_assoc()) {
+      $json[] = array(
+        "id" => $ask["id_pregunta"],
+        "nombre" => $ask["nombre"],
+        "apellido" => $ask["apellido"],
+        "nickname" => $ask["nickname"],
+        "avatar" => $ask["rutaArchivo"],
+        "avatarName" => $ask["nombreArchivo"],
+        "asunto" => $ask["asunto"],
+        "contenido" => $ask["contenido"],
+        "fecha" => $ask["fechaPreguntada"],
+      );
+    }
+    echo json_encode($json);
+  }
+}
+
+?>
