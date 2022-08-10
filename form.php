a95cb7fc97264e9c4516839d673d1823ee1f69cf
Maria3k <maria3kmorales05@gmail.com>
2022-08-10 16:17:03 -0300

    modificaciones

--- form.php
+++ form.php
@@ -130,6 +130,7 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="assets/js/bootstrap.js"></script>
+<script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
 <script>
     function register() {
         document.getElementById("login").style.right = "360px";
