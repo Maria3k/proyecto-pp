51d7cb799c2230258ea6f98737d90a825e41eb91
Pistak41 <66039497+Pistak41@users.noreply.github.com>
2022-08-10 17:00:14 -0300

    Subo parte del login mejorado

--- test/tes.php
+++ test/tes.php
@@ -13,7 +13,19 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="myLeftCtn">
-                        <form class="myForm text-center">
+                      <div class="form-group text-center">
+                          <div class="form-box">
+                              <div class="button-box">
+                                  <button id="login-btn" type="button" class="toggle-btn" onclick="login()">
+                                    <div id="btn"><p></p></div>
+                                  </button>
+                                  <button id="register-btn" type="button" class="toggle-btn" onclick="register()">
+                                    Registrar
+                                  </button>
+                              </div>
+                          </div>
+                      </div>
+                      <form class="myForm text-center">
                             <header>Create new account</header>
                             <div class="form-group">
                                 <i class="fas fa-user"></i>
@@ -40,6 +52,7 @@
                             <input type="submit" class="butt" value="CREATE ACCOUNT">
 
                         </form>
+
                     </div>
                 </div>
                 <div class="col-md-6">
@@ -57,7 +70,12 @@
             </div>
         </div>
 </div>
-
+<script type="text/javascript">
+  function login(){
+    document.getElementById("login-btn").innerHTML = "Iniciar sesion";
+    document.getElementById("register-btn").innerHTML = `<div id="btn"><p>Registrar</p></div>`
+  }
+</script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
