e071775277ec6ba7c10b9d274d2e57a6b7446a76
Maria3k <maria3kmorales05@gmail.com>
2022-08-24 17:43:06 -0300

    detentar el navegador

--- assets/sql/e_t32r.sql
+++ assets/sql/e_t32r.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `e.t32r`
 --
+CREATE DATABASE IF NOT EXISTS `e.t32r` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `e.t32r`;
 
 -- --------------------------------------------------------
 
--- seguridad.php
+++ seguridad.php
@@ -6,6 +6,7 @@
   $enviar = $con->query($query)or die("error de sintaxtis");
   $datos = $enviar->fetch_assoc();
 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,7 +46,22 @@
     <div class="seguridad">
 
         <?php
-            print_r($datos);
+        if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
+           echo 'Internet explorer';
+         elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) //For Supporting IE 11
+            echo 'Internet explorer';
+         elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
+           echo 'Mozilla Firefox';
+         elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
+           echo 'Google Chrome';
+         elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE)
+           echo "Opera Mini";
+         elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE)
+           echo "Opera";
+         elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE)
+           echo "Safari";
+         else
+           echo 'Something else';
 
         ?>
 
