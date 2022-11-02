b41f1e01891ee46462b3e511c9e48d2c6e5537b7
Maria3k <maria3kmorales05@gmail.com>
2022-11-02 17:43:22 -0300

    modificacion de diseño
    modificacion de diseño

--- register.php
+++ register.php
@@ -1,153 +1,99 @@
 <?php
-include("conexion.php");
+include 'conexion.php';
 
-if (isset($_POST["submit"])) {
+header("Content-Type: application/json");
 
-  $nombre = $_POST["nombre"];
-  $apellido = $_POST["apellido"];
-  $user = $_POST["username"];
-  $email = $_POST["email"];
-  $telefono = $_POST["telefono"];
+if ($_POST) {
+  $nombre = $_POST['nombre'];
+  $apellido = $_POST['apellido'];
+  $nickname = $_POST['nickname'];
+  $email = $_POST['email'];
+  $avatar = $_POST['imgInput'];
+  $_POST['m'] = $_POST['m'] + 1;
+  $fechaNacimiento_sql = $_POST['a'] . "-" . $_POST['m'] . "-" . $_POST['d'];
   $contraseña = md5($_POST['contraseña']);
-  $imagen = $_POST["imagen"];
 
-  $envio = $con->query("SELECT * FROM usuario WHERE nickname = '$user' or email = '$email'");
-  $datos = $envio->fetch_assoc();
+  $json = array();
 
-  if(($envio->num_rows) == 0){
-    if($imagen){
-      $query = $con->query("INSERT INTO usuario(nombre, apellido, nickname, email, telefono, contraseña, nAvatar,rol) VALUES ('$nombre','$apellido','$user','$email','$telefono','$contraseña','$imagen',1)") or die("Error en el insert --->".$query.mysqli_error($con));
-      $send = $con->query("SELECT * FROM usuario WHERE email = '$email'");
-      $col = $send->fetch_assoc();
+  if ($_POST['contraseña'] != $_POST['contraconfi']) $json[] = array("passwordError" => '<div class="error"><i class="fa-solid fa-circle-exclamation"></i> La contraseña no coinciden </div>');
 
-      $con->close();
-
-      session_start();
-      $_SESSION = $col;
-      $_SESSION['contraseña'] = $_POST['contraseña'];
-      header("Location:index.php");
+  $verificar_email = "SELECT * FROM usuario WHERE email = '$email' ";
+  $vemail = $con->query($verificar_email);
+  if (($vemail->num_rows) > 0) {
+    $json[] = array("errorEmail" => '<div class="error"><i class="fa-solid fa-circle-exclamation"></i> Este email ya esta registrado</div>');
+  }
+  $verificar_nickname = "SELECT * FROM usuario WHERE nickname = '$nickname' ";
+  $venickame = $con->query($verificar_nickname);
+  if (($venickame->num_rows) > 0) {
+    $json[] = array("errorUsuario" => '<div class="error"><i class="fa-solid fa-circle-exclamation"></i> Este nombre de usuario ya esta registrado</div>');
+  }
 
 
-    }else{
-      echo "SELECCIONE UNA IMAGEN!!!";
-    }
+  if (count($json) == 0) {
+    $i = 0;
+    $query = "INSERT INTO usuario(nombre,apellido,nickname,email,fechaNacimiento,contraseña,nAvatar,rol)
+    VALUES('$nombre','$apellido','$nickname','$email','$fechaNacimiento_sql','$contraseña','$avatar',1)";
+    echo $i++;
+    $insert = $con->query($query) or die("0");
+    echo $i++;
 
-  }else{
-    echo "YA EXISTE UN USUARIO CON ESE NOMBRE O CORREO ELECTRONICO, TARADO :)";
+    session_start();
+    echo $i++;
+
+    $newUser = "SELECT * FROM usuario WHERE email ='$email' and contraseña='$contraseña'";
+    echo $i++;
+
+    $newUserData = $con->query($newUser)->fetch_assoc();
+    echo $i++;
+
+    $navegador = "";
+    echo $i++;
+    $os = "";
+    echo $i++;
+    $fecha = "";
+    echo $i++;
+
+
+    if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
+      $navegador = 'Internet explorer';
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) //For Supporting IE 11
+      $navegador = 'Internet explorer';
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
+      $navegador = 'Mozilla Firefox';
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
+      $navegador = 'Google Chrome';
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE)
+      $navegador = "Opera Mini";
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE)
+      $navegador = "Opera";
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE)
+      $navegador = "Safari";
+    else
+      $navegador = 'Something else';
+
+    $os = php_uname("s");
+    $os += php_uname("r");
+    echo $i++;
+
+    $fecha = (new \DateTime())->format('Y-m-d H:i:s');
+    echo $i++;
+
+    $sesion = "INSERT INTO sesion(id_usuario,navegador,dispostivo,hora) VALUES (" . $newUserData["id_usuario"] . ", '$navegador', '$os',$fecha) ";
+    $send = $con->query($sesion) or die("ERROR" . mysqli_error($con));
+    echo $i++;
+
+
+    $lastSession = "SELECT * FROM sesion WHERE id_usuario = " . $_SESSION["id_usuario"];
+    $filaSession = $con->query($lastSession)->fetch_assoc() or die("ERROR" . mysqli_error($con));
+    echo $i++;
+
+    $_SESSION = $newUserData;
+    array_push($_SESSION, $filaSession);
+    echo $i++;
+
+    print_r($insert);
+  } else {
+
+    echo json_encode($json);
   }
 }
-
-?>
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="assets/img/favicon/favicon.png">
-  <link rel="stylesheet" href="assets/css/login.css">
-  <link rel="stylesheet" href="assets/css/bootstrap.css">
-  <link rel="stylesheet" href="assets/css/register.css">
-  <title>Registrarse</title>
-</head>
-
-<body>
-  <div id="formulario" class="container">
-    <div class="row">
-      <div id="logo" class="col-4">
-        <img src="assets/img/iconos/logoblanco.png" alt="logo.png">
-      </div>
-      <div class="col-8">
-        <div id="form" class="row">
-          <div class="col-12">
-            <h2 style="text-align: center;">REGISTER</h2>
-          </div>
-          <div class="col-12">
-            <form action="register.php" method="post">
-              <div class="form-group">
-                <div class="row">
-                  <div class="col-6">
-                    <input type="text" name="nombre" placeholder="Nombre" required>
-                  </div>
-                  <div class="col-6">
-                    <input type="text" name="apellido" placeholder="Apellido" required>
-                  </div>
-                </div>
-                <input type="text" name="username" placeholder="Nombre de usuario" required>
-              </div>
-              <div class="form-group">
-                <input type="email" name="email" placeholder="Correo electronico" required>
-                <input type="text" name="cemail" placeholder="Correo electronico nuevamente" required>
-              </div>
-              <div class="form-group">
-                <input type="tel" name="telefono" placeholder="Ingrese su numero de telefono" required>
-              </div>
-              <div class="form-group">
-                <input type="password" name="contraseña" placeholder="Ingrese su contraseña" required>
-                <input type="password" name="ccontraseña" placeholder="Ingrese su contraseña nuevamente" required>
-              </div>
-              <div class="form-group">
-                <p> Seleccione una imagen de perfil</p>
-
-                <select name="imagen" hidden>
-                  <option id="option"></option>
-                </select>
-
-
-                <ul id="lista">
-
-
-
-                  <?php
-                  $envio = $con->query("SELECT * FROM avatar");
-                  while ($row = $envio->fetch_assoc()) {
-
-                  ?>
-
-                    <li>
-                      <img id=<?php echo $row["id_avatar"]; ?> src=<?php echo $row["rutaArchivo"]; ?> alt=<?php echo $row["nombreArchivo"]; ?> onclick="<?php echo "select(", $row["id_avatar"], ")"; ?>" width="75px" height="75px">
-                    </li>
-
-                  <?php
-
-                  }
-
-                  ?>
-                  <!--
-                    <input type="radio" name="" value="">
-                    <img src=alt="<?php echo $row["nombreArchivo"]; ?>" width="75px" height="75px">
-                  -->
-
-
-                </ul>
-              </div>
-              <div class="form-group">
-                <input id="submit" type="submit" name="submit" style="width: auto;">
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <script src="assets/js/bootstrap.js"></script>
-  <script>
-    function select(n) {
-      if (document.getElementById(n).hasAttribute("class")) {
-        document.getElementById(n).removeAttribute("class");
-        document.getElementById("option").removeAttribute("value");
-      } else {
-        for (var i = 1; i <= document.getElementById("lista").childElementCount; i++) {
-          if (document.getElementById(i).hasAttribute("class")) {
-            document.getElementById(i).removeAttribute("class");
-          }
-        }
-        document.getElementById("option").setAttribute("value", n);
-        document.getElementById(n).setAttribute("class", "seleccionado");
-      }
-    }
-  </script>
-</body>
-
-</html>
