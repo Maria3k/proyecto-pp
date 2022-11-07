cd7cde36df5584c8a6b37d95242910a9f7bf8d70
Maria3k <maria3kmorales05@gmail.com>
2022-11-07 04:05:25 -0300

    tarjeta de infoGeneral
    tarjeta de infoGeneral

---
+++ infoGeneralcopy.php
@@ -0,0 +1,184 @@
+<?php
+include("conexion.php");
+session_start();
+if ($_SESSION) {
+  $usr = $con->query("SELECT usuario.*, avatar.* FROM usuario LEFT JOIN avatar ON usuario.nAvatar = avatar.id_avatar WHERE id_usuario = " . $_SESSION["id_usuario"])->fetch_assoc();
+} else {
+  header("location:login.php");
+}
+
+$menu = '';
+
+if ($_SESSION) {
+
+  $nAvatar = $_SESSION["nAvatar"];
+
+  $send = $con->query("SELECT * FROM avatar WHERE id_avatar = $nAvatar");
+  $col = $send->fetch_assoc();
+  $ad = '';
+
+  if ($_SESSION["rol"] == 2) {
+    $ad = '<li><a href="backend.php"><i class="fa-solid fa-gear"></i>Editar</a></li>';
+  }
+
+
+  $menu = '        
+  <div class="action" >
+    <div class="profile" onclick="menuToggle()">
+      <img src="' . $col["rutaArchivo"] . '" width="30px" height="30px" alt="' . $col["nombreArchivo"] . '">
+    </div>
+    <div class="menu">
+      <ul>
+        <li id="primero"><a href="perfil.php"><i class="fa-solid fa-user"></i>Perfil</a></li>
+        ' . $ad . '
+        <li><a href="logout.php"><i class="fa-solid fa-arrow-right-from-bracket"></i>Cerrar Sesion</a></li>
+      </ul>
+    </div>
+  </div>
+        ';
+} else {
+  $menu = '<a class="btn-nav" href="register.php">Register</a><a class="btn-nav" href="login.php">Iniciar Sesion</a>';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="assets/css/perfilcopy.css">
+  <link rel="stylesheet" href="accordiancopy.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="assets/css/bootstrapcopy.css">
+  <link rel="stylesheet" href="assets/css/iconscopy.css">
+  <link rel="stylesheet" href="assets/css/fondo.css">
+  <link rel="stylesheet" href="infoGeneral.css">
+  <style>
+    .option:hover {
+      color: white !important;
+    }
+
+    .option span {
+      opacity: 0;
+      transition-duration: 250ms;
+    }
+
+    .option:hover>span {
+      opacity: 1;
+    }
+
+    .activeO a {
+      color: white !important;
+    }
+  </style>
+</head>
+
+<body>
+  <nav>
+    <img class="img1" src="assets/img/escuela/loguito.png" alt="loguito.png">
+    <div class="icon-wrapper" data-numbrer="1">
+      <img src="assets/img/iconos/bell-solid.svg" class="bell-icon">
+    </div>
+    <?= $menu ?>
+  </nav>
+  <!--menu -->
+
+  <div id="accordian">
+    <ul class="show-dropdown">
+      <li>
+        <a id="option1" class="option" href="perfilcopy.php">
+          <i class="fa fa-tachometer icon"></i>
+          <span>Inicio</span>
+        </a>
+      </li>
+      <li>
+        <a class="option" href="infoPersonalcopy.php">
+          <i class="fa fa-address-book icon"></i>
+          <span>Info Personal</span>
+        </a>
+      </li>
+      <li>
+        <a class="option" href="seguridadcopy.php">
+          <i class="fa-solid fa-lock icon"></i>
+          <span>Seguridad</span>
+        </a>
+      </li>
+      <li class="activeO">
+        <a>
+          <i class="fa fa-clone icon"></i>
+          <span>Info General</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <!-- Que ofrece la web -->
+  <section>
+    <div class="cont-title">
+      <label>¿Qué ofrece la web?</label>
+    </div>
+    <div class="info">
+      <label>E.T32 Respuestas está optimizada para la resolución de incertidumbres de los estudiantes con respecto a sus especialidad; donde los directivos, profesores y/o estudiantes que poseen el conocimiento para responder la interrogante, a fin de que la duda sea resuelta.</label>
+    </div>
+    <div id="cont-title" >
+      <label>Directivos</label>
+    </div>
+    <div id="info">
+
+    </div>
+    
+  </section>
+  <!--  -->
+
+  <!-- Aca va el footer -->
+  <!-- Script del accordian -->
+  <script>
+    // ---------vertical-menu with-inner-menu-active-animation-----------.
+    var tabsVerticalInner = $('#accordian');
+    var selectorVerticalInner = $('#accordian').find('li').length;
+    var activeItemVerticalInner = tabsVerticalInner.find('.active');
+    var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
+    var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
+    var itemPosVerticalTop = activeItemVerticalInner.position();
+    var itemPosVerticalLeft = activeItemVerticalInner.position();
+    $(".selector-active").css({
+      "top": itemPosVerticalTop.top + "px",
+      "left": itemPosVerticalLeft.left + "px",
+      "height": activeWidthVerticalHeight + "px",
+      "width": activeWidthVerticalWidth + "px"
+    });
+    $("#accordian").on("click", "li", function(e) {
+      $('#accordian ul li').removeClass("active");
+      $(this).addClass('active');
+      var activeWidthVerticalHeight = $(this).innerHeight();
+      var activeWidthVerticalWidth = $(this).innerWidth();
+      var itemPosVerticalTop = $(this).position();
+      var itemPosVerticalLeft = $(this).position();
+      $(".selector-active").css({
+        "top": itemPosVerticalTop.top + "px",
+        "left": itemPosVerticalLeft.left + "px",
+        "height": activeWidthVerticalHeight + "px",
+        "width": activeWidthVerticalWidth + "px"
+      });
+    });
+
+    function infoPersonal() {
+      setTimeout("location.href = 'infoPersonal.php';", 400);
+    }
+
+    function perfil() {
+      setTimeout("location.href = 'perfil.php';", 400);
+    }
+
+    function seguridad() {
+      setTimeout("location.href = 'seguridad.php';", 400);
+    }
+  </script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
+</body>
+
+</html>
