72e5965cfe7010d490299e564dd2bcc79839c03e
Ignacio Rios Lahore <ignaciorioslahore@gmail.com>
2022-11-11 17:37:08 -0300

    Cambio conexion SQL a servidor remoto
    Cambio conexion SQL a servidor remoto

--- conexion.php
+++ conexion.php
@@ -1,8 +1,9 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "e.t32r";
 
-    $con =  new mysqli($host, $user, $password, $database) or die("No pudo conectarse: ". mysqli_error($con));
-?>
+$host = 'containers-us-west-115.railway.app';
+$user = 'root';
+$password = 'NdJnyRORxukfIdGfFzO6';
+$database = 'railway';
+$port = 6523;
+
+$con =  new mysqli($host, $user, $password, $database, $port) or die('No pudo conectarse: ' . mysqli_error($con));
