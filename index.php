db8100a08d9bb4033c3e2956ea17549eac4671ee
thrispao31 <thristal.guerra.et32@gmail.com>
2022-11-15 22:36:51 -0300

    index
    subida de todo el index

--- index.php
+++ index.php
@@ -51,9 +51,9 @@ if ($_SESSION) {
   <meta property="og:image:height" content="300px" />
   <link rel="icon" href="assets/img/favicon/favicon.png">
   <link rel="stylesheet" href="assets/css/index.css">
-  <link rel="stylesheet" href="assets/css/bootstrap.css">
-  <link rel="stylesheet" href="assets/css/icons.css">
-  <link rel="stylesheet" href="assets/css/especialidades.css">
+  <link rel="stylesheet" href="assets/css/tarjetas.css">
+  <link rel="stylesheet" href="assets/css/footer.css">
+  <link rel="stylesheet" href="assets/css/slider.css">
   <title>E.T.32 Preguntas</title>
 </head>
 
@@ -66,35 +66,136 @@ if ($_SESSION) {
     <?= $menu ?>
   </nav>
   <div class="container">
-    <div class="row align-item-center">
-      <div class="col-4">
-        <img class="img-fluid" src="assets/img/escuela/escuela.jpg" alt="escuela.jpg">
+    <h1>HISTORIA DE LA <span>ET32</span></h1>
+    <br>
+    <div class="process-wrapper">
+      <div id="progress-bar-container">
+        <ul>
+          <li class="step step01 active">
+            <div class="step-inner">ESC. FÁBRICA</div>
+          </li>
+          <li class="step step02">
+            <div class="step-inner">ACTO</div>
+          </li>
+          <li class="step step03">
+            <div class="step-inner">ESC. MUNICIPAL</div>
+          </li>
+          <li class="step step04">
+            <div class="step-inner">ESC. TÉCNICA</div>
+          </li>
+          <li class="step step05">
+            <div class="step-inner">LEMA</div>
+          </li>
+        </ul>
+
+        <div id="line">
+          <div id="line-progress"></div>
+        </div>
       </div>
-      <div class="col-6">
-        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus mollitia similique non atque nesciunt velit recusandae a reprehenderit cum enim quasi perferendis aperiam dolorum magnam, aliquam laudantium quidem, animi qui. </p>
+
+      <div id="progress-content-section">
+        <div class="section-content factory active">
+          <h2>Escuela Fábrica</h2>
+          <p>
+            En agosto de 1950 se inaugura como Escuela Fábrica 131 "Géneral José De San Martín".
+          </p>
+        </div>
+        <div class="section-content act">
+          <h2>Acto Inaugural</h2>
+          <p>
+            22 de agosto de 1950 se realizó el acto inaugural, con la presencia del entonces Presidente de la Nación,
+            General Juan Domingo Perón.
+          </p>
+        </div>
+        <div class="section-content municipal">
+          <h2>Escuela Municipal</h2>
+          <p>
+            En 1992 la escuela pasó a la Administración del ámbito de la Municipalidad de la Ciudad de Buenos Aires
+            cambiando su denominación a Escuela Municipal de Enseñanza Técnica 2 "General José de San Martín".
+          </p>
+        </div>
+        <div class="section-content technique">
+          <h2>Escuela Técnica</h2>
+          <p>
+            En 1994 pasó a ser Escuela Técnica 32 Distrito Escolar 14 "General José de San Martín" hasta la actualidad.
+          </p>
+        </div>
+        <div class="section-content motto">
+          <h2>Lema de la ET32</h2>
+          <p>
+            Somos el azul de nuestros guardapolvos de Técnica; somos el equipo: indestructibles engranajes metálicos
+            unidos en eterno movimiento; somos vivos rayos, energía que dan potencia y dinamismo a magna labor, somos el
+            volante que rodea y direcciona al objetivo; somos la placa impresa, base que sostiene y que transmite plenas
+            conexiones para que todo funcione ¡somos escuela pública y técnic allí donde reside la patria misma... somos
+            la 32!
+          </p>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="Contenedor">
-    <div class="card">
-      <a href="computacion.php" title="Sección de Computacion"><img src="assets/img/escuela/computacion.png" alt="computacion.png"></a>
-      <h1>Computacion</h1>
     </div>
 
-    <div class="card">
-      <a href="mecanica.php" title="Sección de Mecanica"><img src="assets/img/escuela/mecanica.png" alt="mecanica.png"></a>
-      <h1>Mecanica</h1>
+
+    <!--TARJETAS-->
+    <div class="cont-card">
+      <a href="automotores.html" class="card">
+        <div class="face front">
+          <img src=".//assets/img/escuela/automotores.png" alt="">
+          <h3>Automotores</h3>
+        </div>
+        <div class="face back">
+          <h3>Automotores</h3>
+          <p>Es una espcialidad que se focaliza en el estudio y analisis de todo lo referente a los vehiculos
+            automotores, desde la mecánica hasta la dinámica de los mismos</p>
+          <div class="link">
+            ¿Deseas Preguntar Algo?
+          </div>
+        </div>
+      </a>
+      <a href="computacion.html" class="card">
+        <div class="face front">
+          <img src=".//assets/img/escuela/computacion.png" alt="">
+          <h3>Computación</h3>
+        </div>
+        <div class="face back">
+          <h3>Computación</h3>
+          <p> Es la tecnologia desarrollada para el tratamiento automático de la informacion mediante el uso de
+            computadoras </p>
+          <div class="link">
+            ¿Deseas Preguntar Algo?
+          </div>
+        </div>
+      </a>
+      <a href="mecanica.html" class="card">
+        <div class="face front">
+          <img src=".//assets/img/escuela/mecanica.png" alt="">
+          <h3>Mecánica</h3>
+        </div>
+        <div class="face back">
+          <h3>Mecánica</h3>
+          <p> Es la ciencia que estudia el movimiento de los cuerpos bajo la accion de las fuerzas participantes.</p>
+          <div class="link">
+            ¿Deseas Preguntar Algo?
+          </div>
+        </div>
+      </a>
     </div>
 
-    <div class="card">
-      <a href="automotores.php" title="Sección de Automotores"><img src="assets/img/escuela/automotores.png" alt="automotores.png"></a>
-      <h1>Automotores</h1>
+    <div class="body">
+      <div class="wrapper">
+        <i id="left" class="fa-solid fa-angle-left"></i>
+        <div class="carousel">
+          <img src="assets/img/escuela/aaa.jpg" alt="img" draggable="false">
+          <img src="assets/img/escuela/entrada.jpg" alt="img" draggable="false">
+          <img src="assets/img/escuela/escuela.jpg" alt="img" draggable="false">
+          <img src="assets/img/escuela/frente.jpg" alt="img" draggable="false">
+          <img src="assets/img/escuela/imgE.jpg" alt="img" draggable="false">
+        </div>
+        <i id="right" class="fa-solid fa-angle-right"></i>
+      </div>
     </div>
-  </div>
-  <div class="contenedorVideo">
-    <video src="assets/video/3 Ejercicios Para Saltar Mas En Casa(Prueba).mp4" class="slider" loop muted controls></video>
-  </div>
-  <footer class="foot">
+    
+
+    <!--FOOTER-->
+    <footer class="foot">
       <div class="grupo-1">
         <div class="box">
           <figure>
@@ -102,39 +203,45 @@ if ($_SESSION) {
               <img src="./assets/img/iconos/logoblanco.png" alt="">
             </a>
           </figure>
-         <!--<h4>Contacto</h4>
-          <ul>
-            <li>Email DOE: ofdealumnos32@gmail.com</li>
-            <li>Teléfonos: 4551-9121 4555-4026/4034</li>
-            <li>Dirección: Teodoro García 3899, C1427ECG CABA</li>
-          </ul> --> 
         </div>
         <div class="box">
-         <h5>Autores:</h5> 
-              <ul>
-                <li>Maria Morales - 6°1 Computacion</li>
-                <li>Nicolas Domeq - 6°1 Computacion</li>
-                <li>Ignacio Rios Lahore - 6°1 Computacion</li>
-                <li>Thristall Guerra - 6°1 Computación</li>
-                <li>Ivan Britez - 6°1 Computación</li>
-              </ul>
+          <h5>Autores:</h5>
+          <ul>
+            <li>Maria Morales - 6°1 Computacion</li>
+            <li>Nicolas Domeq - 6°1 Computacion</li>
+            <li>Ignacio Rios Lahore - 6°1 Computacion</li>
+            <li>Thristall Guerra - 6°1 Computación</li>
+            <li>Ivan Britez - 6°1 Computación</li>
+          </ul>
         </div>
         <div class="box">
           <h5>Redes Sociales</h5>
           <div class="connect">
-            <a href="https://www.facebook.com/groups/tecnica32/" rel="noopener noreferrer" class="facebook" target="_blank" title="Facebook"><!--LINK DE FACEBOOK -->
-              <i class="fab fa-facebook-f"></i> <!--ICONO DE FACEBOOK -->
+            <a href="https://www.facebook.com/groups/tecnica32/" rel="noopener noreferrer" class="facebook"
+              target="_blank" title="Facebook">
+              <!--LINK DE FACEBOOK -->
+              <i class="fab fa-facebook-f"></i>
+              <!--ICONO DE FACEBOOK -->
             </a>
-            <a href="https://github.com/Maria3k/Proyecto-PP" rel="noopener noreferrer" class="github" target="_blank" title="Github"><!--LINK DE GITHUB -->
-              <i class="fab fa-github"></i> <!--ICONO DE GITHUB -->
-            </a>
-         
-            <a href="https://www.youtube.com/channel/UCywUijMchnujSg6dVVUKprQ/videos?view_as=subscriber" rel="noopener noreferrer" class="youtube" target="_blank" title="Youtube"><!--LINK DE YOUTUBE -->
-              <i class="fab fa-youtube"></i> <!--ICONO DE YOUTUBE -->
+            <a href="https://github.com/Maria3k/Proyecto-PP" rel="noopener noreferrer" class="github" target="_blank"
+              title="Github">
+              <!--LINK DE GITHUB -->
+              <i class="fab fa-github"></i>
+              <!--ICONO DE GITHUB -->
             </a>
 
-            <a href="https://www.instagram.com/la_gloriosa_32_escuela_tecnica/?hl=es-la" rel="noopener noreferrer" class="instagram" target="_blank" title="Instagram"><!--LINK DE INSTAGRAM -->
-              <i class="fab fa-instagram"></i><!--ICONO DE INSTAGRAM -->
+            <a href="https://www.youtube.com/channel/UCywUijMchnujSg6dVVUKprQ/videos?view_as=subscriber"
+              rel="noopener noreferrer" class="youtube" target="_blank" title="Youtube">
+              <!--LINK DE YOUTUBE -->
+              <i class="fab fa-youtube"></i>
+              <!--ICONO DE YOUTUBE -->
+            </a>
+
+            <a href="https://www.instagram.com/la_gloriosa_32_escuela_tecnica/?hl=es-la" rel="noopener noreferrer"
+              class="instagram" target="_blank" title="Instagram">
+              <!--LINK DE INSTAGRAM -->
+              <i class="fab fa-instagram"></i>
+              <!--ICONO DE INSTAGRAM -->
             </a>
           </div>
         </div>
@@ -144,14 +251,45 @@ if ($_SESSION) {
         <small>&copy; 2022 <b>ET32 RESPUESTAS</b> - Todos los derechos reservados.</small>
       </div>
     </footer>
-  <script src="assets/js/bootstrap.js"></script>
-  <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
-  <script>
-    function menuToggle() {
-      const toggleMenu = document.querySelector('.menu');
-      toggleMenu.classList.toggle('active')
-    }
-  </script>
+
+    <script src="https://kit.fontawesome.com/b3b892b65b.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="assets/js/slider.js"></script>
+
+    <script>
+      $(".step").click(function () {
+        $(this).addClass("active").prevAll().addClass("active");
+        $(this).nextAll().removeClass("active");
+      });
+
+      $(".step01").click(function () {
+        $("#line-progress").css("width", "3%");
+        $(".factory").addClass("active").siblings().removeClass("active");
+      });
+
+      $(".step02").click(function () {
+        $("#line-progress").css("width", "25%");
+        $(".act").addClass("active").siblings().removeClass("active");
+      });
+
+      $(".step03").click(function () {
+        $("#line-progress").css("width", "50%");
+        $(".municipal").addClass("active").siblings().removeClass("active");
+      });
+
+      $(".step04").click(function () {
+        $("#line-progress").css("width", "75%");
+        $(".technique").addClass("active").siblings().removeClass("active");
+      });
+
+      $(".step05").click(function () {
+        $("#line-progress").css("width", "100%");
+        $(".motto").addClass("active").siblings().removeClass("active");
+      });
+    </script>
+
 </body>
 
 </html>
